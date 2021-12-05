2a2a42182f2d0f1172924555c2bccdca5ee5f136
js2docker <44265853+js2docker@users.noreply.github.com>
2021-12-05 01:55:00 -0500

    changed chainID

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -29,8 +29,8 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [3, 1287]
-  //supportedChainIds: [1, 3, 4, 5, 42, 1287]
+  supportedChainIds: [1]
+  //supportedChainIds: [1]
 })
 
 // mainnet only
